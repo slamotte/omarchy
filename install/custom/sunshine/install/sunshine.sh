7bfbcd316b926f8ed6d1b6ebc52ffad4bc0611e5
Steve Lamotte <steve@lexor.ca>
2025-09-24 22:03:58 -0500

    Forgot to install Sunshine ffs :facepalm:. Also fix a couple typos

--- install/custom/sunshine/install/sunshine.sh
+++ install/custom/sunshine/install/sunshine.sh
@@ -1,3 +1,18 @@
+echo "Installing Sunshine from LizardByte repository..."
+if ! grep -q "^\[lizardbyte\]" /etc/pacman.conf; then
+  echo "Adding LizardByte repository to pacman..."
+  sudo cat <<EOF >> /etc/pacman.conf
+
+[lizardbyte]
+SigLevel = Optional
+Server = https://github.com/LizardByte/pacman-repo/releases/latest/download
+EOF
+  sudo pacman -Sy
+fi
+
+echo "Installing Sunshine..."
+sudo pacman -S sunshine --noconfirm
+
 echo "Downloading Sunshine icon..."
 curl --silent --output ~/.local/share/applications/icons/sunshine-config.png \
   https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/sunshine.png
@@ -11,7 +26,7 @@ Comment=Sunshine Configuration UI
 Exec=omarchy-launch-webapp https://localhost:47990
 Terminal=false
 Type=Application
-Icon=/home/steve/.local/share/applications/icons/sunshine-config.png
+Icon=~/.local/share/applications/icons/sunshine-config.png
 StartupNotify=true
 EOF
 
@@ -30,7 +45,7 @@ echo "Adding notification to configure Sunshine on first run..."
 sed -i '1inotify-send -u critical "☀️ Configure Sunshine" "Click here to configure Sunshine."' $OMARCHY_INSTALL/first-run/welcome.sh
 
 # Add handler to mako notifications to open Sunshine config in browser
-tee ~/.local/share/omarchy/default/mako/core.ini <<'EOF'
+tee -a ~/.local/share/omarchy/default/mako/core.ini <<'EOF'
 
 [summary~="Configure Sunshine"]
 on-button-left=exec sh -c 'omarchy-notification-dismiss "Configure Sunshine"; omarchy-launch-browser https://localhost:47990'
