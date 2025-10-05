85065c81f93fda3e974ca0d8dd738658f4484611
Steve Lamotte <steve@lexor.ca>
2025-10-05 17:32:35 -0500

    Remove debugging logs

--- install/custom/first-run.sh
+++ install/custom/first-run.sh
@@ -1,12 +1,4 @@
 # Find and execute all custom installation scripts
-echo "CUSTOM_PATH is set to: $CUSTOM_PATH" >> $CUSTOM_LOGS
-echo "Searching for custom first-run scripts in $CUSTOM_PATH..." >> $CUSTOM_LOGS
-echo "CWD is $(pwd)" >> $CUSTOM_LOGS
-echo "Expecting to find the following files:" >> $CUSTOM_LOGS
-find "$CUSTOM_PATH" -mindepth 2 -type f -name "first-run.sh" >> $CUSTOM_LOGS 2>&1
-echo "----------------------------------------------" >> $CUSTOM_LOGS
 for f in $(find "$CUSTOM_PATH" -mindepth 2 -type f -name "first-run.sh"); do
-  echo "Executing custom first-run script: $f" >> $CUSTOM_LOGS
   source "$f"
-  echo "Finished custom first-run script: $f" >> $CUSTOM_LOGS
 done
--- install/custom/sunshine/first-run.sh
+++ install/custom/sunshine/first-run.sh
@@ -1,11 +1,2 @@
-echo "Before UFW status:" >> $CUSTOM_LOGS
-sudo ufw status verbose >> $CUSTOM_LOGS 2>&1
-echo "----------------------------------------------" >> $CUSTOM_LOGS
-echo "Adding Sunshine profile to UFW..." >> $CUSTOM_LOGS
-sudo ufw app update Sunshine >> $CUSTOM_LOGS 2>&1
-echo "----------------------------------------------" >> $CUSTOM_LOGS
-echo "Allowing Sunshine in UFW..." >> $CUSTOM_LOGS
-sudo ufw allow Sunshine >> $CUSTOM_LOGS 2>&1
-echo "----------------------------------------------" >> $CUSTOM_LOGS
-echo "After UFW status:" >> $CUSTOM_LOGS
-sudo ufw status verbose >> $CUSTOM_LOGS 2>&1
+sudo ufw app update Sunshine
+sudo ufw allow Sunshine
