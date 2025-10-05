10980e33825025d318590b0c350fbe3b3f10c6a6
Steve Lamotte <steve@lexor.ca>
2025-10-05 12:59:07 -0500

    More logging

--- install/custom/first-run.sh
+++ install/custom/first-run.sh
@@ -1,4 +1,6 @@
 # Find and execute all custom installation scripts
 for f in $(find "$OMARCHY_INSTALL/custom" -mindepth 2 -type f -name "first-run.sh"); do
+  echo "Executing custom first-run script: $f" >> $CUSTOM_LOGS
   source "$f"
+  echo "Finished custom first-run script: $f" >> $CUSTOM_LOGS
 done
