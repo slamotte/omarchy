2ee692593c384b7ef58866408c0a80528707ab9a
Steve Lamotte <steve@lexor.ca>
2025-10-14 00:29:13 -0500

    Add first-run logging

--- install/first-run/firewall.sh
+++ install/first-run/firewall.sh
@@ -1,3 +1,5 @@
+echo "Start of firewall.sh..." >> /var/log/first-run-firewall.log
+
 # Allow nothing in, everything out
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
@@ -13,14 +15,22 @@ sudo ufw allow 22/tcp
 sudo ufw allow in proto udp from 172.16.0.0/12 to 172.17.0.1 port 53 comment 'allow-docker-dns'
 
 # Turn on the firewall
-sudo ufw enable
+echo Enabling UFW firewall... >> /var/log/first-run-firewall.log
+sudo ufw enable >> /var/log/first-run-firewall.log 2>&1
 
 # Turn on Docker protections
-sudo ufw-docker install
-sudo ufw reload
+echo Enabling UFW Docker protections... >> /var/log/first-run-firewall.log
+sudo ufw-docker install >> /var/log/first-run-firewall.log 2>&1
+
+echo Reloading ufw... >> /var/log/first-run-firewall.log
+sudo ufw reload >> /var/log/first-run-firewall.log 2>&1
 
 # Maybe restart everything?
-sudo systemctl restart ufw docker
+echo Restarting ufw and Docker... >> /var/log/first-run-firewall.log
+sudo systemctl restart ufw docker >> /var/log/first-run-firewall.log 2>&1
 
 # Enable systemd service to start on boot
-sudo systemctl enable ufw
+echo Enabling UFW with systemctl... >> /var/log/first-run-firewall.log
+sudo systemctl enable ufw >> /var/log/first-run-firewall.log 2>&1
+
+echo "End of firewall.sh..." >> /var/log/first-run-firewall.log
